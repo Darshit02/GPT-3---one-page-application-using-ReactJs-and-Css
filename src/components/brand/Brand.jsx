29b033f4ca4b36b7f92cd84279395461f4321fa4
Darshit <darshitmistry2@gmail.com>
2022-10-25 12:51:22 +0530

    Adding a section called Brand

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import './brand.css'
+import { google, slack, dropbox,atlassian ,shopify} from './import'
 
 const Brand = () => {
   return (
+   <div className="gpt3__brand section__padding">
     <div>
-      Brand
+      <img src={google} alt="google" />
     </div>
+    <div>
+      <img src={slack} alt="slack" />
+    </div>
+    <div>
+      <img src={dropbox} alt="dropbox" />
+    </div>
+    <div>
+      <img src={atlassian} alt="atlassian" />
+    </div>
+    <div>
+      <img src={shopify} alt="shopify" />
+    </div>
+   </div>
   )
 }
 
--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import './whatgpt3.css'
+import { Feature } from '../../components'
 
 const WhatGPT3 = () => {
   return (
-    <div>
-      What is GPT3
+    <div className="gpt3__whatgpt3 section__margin">
+      <div className="gpt3__whatgpt3-feature">
+        <Feature />
+      </div>
+      <div className='gpt3__whatgpt3-header'>
+        <h1 className="gradient-text">The possibilities are beyond your imagination</h1>
+        <p>Explore the Library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+      <Feature />
+      <Feature />
+      <Feature />
+      </div>
     </div>
   )
 }
