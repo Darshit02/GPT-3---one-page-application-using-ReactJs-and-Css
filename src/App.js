71200e4731b7da6a415886ce39545c1a7caf3c73
Darshit <darshitmistry2@gmail.com>
2022-10-25 10:40:03 +0530

    That's a header section was implimented

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import './App.css'
 
-import { Footer,Blog,Possibility ,Features,WhatGPT3,Header} from './containers'
-import {CTA , Brand ,Navbar } from './components'
+import { Footer, Blog, Possibility , Features, WhatGPT3, Header } from './containers'
+import { CTA , Brand , Navbar } from './components'
 const App = () => {
   return (
     <div className="App">
