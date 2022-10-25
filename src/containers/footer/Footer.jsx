f366d9acdb2cd5901bcd9cf55d66cac893bb18c2
Darshit <darshitmistry2@gmail.com>
2022-10-25 19:31:32 +0530

    fixing some image issues

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -14,8 +14,8 @@ const Footer = () => (
 
     <div className="gpt3__footer-links">
       <div className="gpt3__footer-links_logo">
-        <img src={gpt3Logo} alt="gpt3_logo" />
-        <p>Crechterwoord K12 182 DK Alknjkcb, <br /> All Rights Reserved</p>
+        {/* <img src={gpt3Logo} alt="gpt3_logo" /> */}
+        <p> All Rights Reserved</p>
       </div>
       <div className="gpt3__footer-links_div">
         <h4>Links</h4>
@@ -45,4 +45,3 @@ const Footer = () => (
 );
 
 export default Footer;
-
