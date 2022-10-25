123b9a7ecf0b097603edf6c3f1c1ba5d4a06cfc6
Darshit <darshitmistry2@gmail.com>
2022-10-25 18:49:42 +0530

    Adding a possibility section to possibal with An Open Ai

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -12,7 +12,7 @@ const Posssibility = () => {
         <h4>Request Early Access to Get Started</h4>
         <h1 className="gradient__text">The Possibility are <br /> beyond your imagination</h1>
         <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
-        <h4>Request Early Access to Get Started</h4>
+        <h4 id="gpt3__possibility-start">Request Early Access to Get Started</h4>
       </div>
     </div>
   )
